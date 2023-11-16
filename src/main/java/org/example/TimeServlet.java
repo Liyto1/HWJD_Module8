c39a78236ce0943144da0912209686dbe08f10f1
Lev <lev.lvi2@gmail.com>
2023-11-16 20:18:52 +0200

    commit

---
+++ src/main/java/org/example/TimeServlet.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+@WebServlet(value = "/time")
+public class TimeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String timezoneParam = req.getParameter("timezone");
+
+        if (timezoneParam == null) {
+            timezoneParam = "Etc/GMT";
+        } else {
+            timezoneParam = timezoneParam.replace("UTC+", "Etc/GMT-").replace("UTC-", "Etc/GMT+");
+        }
+
+        ZoneId zoneId = ZoneId.of(timezoneParam);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z");
+        ZonedDateTime currentTime = ZonedDateTime.now(zoneId);
+        String formattedTime = currentTime.format(formatter).replace("GMT", "UTC");
+
+        resp.setContentType("text/html; charset=utf-8");
+        resp.getWriter().println("<html><body>");
+        resp.getWriter().println("Current Time: " + formattedTime);
+        resp.getWriter().println("</body></html>");
+        resp.getWriter().close();
+    }
+}
